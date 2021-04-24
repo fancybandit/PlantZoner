15c00393909bdca2278b0281803ebdedd2222aa0
Jacob Klimenko <droidedjake@gmail.com>
2021-04-24 17:31:10 -0400

    Set up associations between models

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,5 +1,5 @@
 class Garden < ApplicationRecord
-  belongs_to :owner
+  belongs_to :owner, class_name: "User"
   belongs_to :growing_zone
   belongs_to :plant
 end
--- app/models/growing_zone.rb
+++ app/models/growing_zone.rb
@@ -1,2 +1,4 @@
 class GrowingZone < ApplicationRecord
+    has_many :gardens
+    has_many :plants, through: :gardens
 end
