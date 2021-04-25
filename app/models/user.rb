ca592874044269b79cd17938c7c9c576dd529454
Jacob Klimenko <droidedjake@gmail.com>
2021-04-25 17:24:27 -0400

    Add has_secure_password to User & bundle gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :gardens, foreign_key: "owner_id"
     has_many :plants, through: :gardens
     has_many :growing_zones, through: :gardens
+
+    has_secure_password
 end
