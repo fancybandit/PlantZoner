d223de009309ee6b66a867c5a062c81aabff70ca
Jacob Klimenko <droidedjake@gmail.com>
2021-04-25 17:39:28 -0400

    Add routes & actions for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :gardens
   resources :plants
   resources :growing_zones
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
