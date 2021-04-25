2681bd040c05693d2ce7e15d80e485d79d00510e
Jacob Klimenko <droidedjake@gmail.com>
2021-04-25 15:54:00 -0400

    Fix association issues with User (Owner)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :gardens
+    has_many :gardens, foreign_key: "owner_id"
     has_many :plants, through: :gardens
     has_many :growing_zones, through: :gardens
 end
