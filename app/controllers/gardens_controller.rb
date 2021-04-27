5ead69e5d8b7fa1f6a9cd8fcf835a898a6a13ca6
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 18:07:34 -0400

    Add GrowingZone controller logic from mini project

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,11 +1,44 @@
 class GardensController < ApplicationController
     before_action :redirect_if_not_logged_in
 
+    def index
+        if params[:user_id]
+            if find_owner
+                @gardens = @owner.gardens
+            else
+                flash[:error] = "The specified user does not exist."
+                @gardens = Garden.all
+            end
+        else
+            @gardens = Garden.all
+        end
+    end
+
     def new
-        @garden = Garden.new
+        if params[:user_id]
+            if find_owner
+                #verify is current_user before building a garden
+                @garden = @owner.gardens.build(garden_params)
+            else
+                flash[:error] = "The specified user does not exist."
+                @garden = Garden.new
+            end
+        else
+            @garden = Garden.new
+        end
     end
 
     def create
+    end
+
+    private
+
+    def garden_params
+        params.require(:garden).permit(:name, :owner_id, :plant_id, :growing_zone_id)
+    end
+
+    def find_owner
+        @owner = User.find_by(id: params[:user_id])
     end
 
 end
--- app/controllers/growing_zones_controller.rb
+++ app/controllers/growing_zones_controller.rb
@@ -1,2 +1,16 @@
 class GrowingZonesController < ApplicationController
+
+    def index
+        @growing_zones = GrowingZone.order(:created_at)
+    end
+
+    def show
+        @growing_zone = GrowingZone.find_by(id: params[:id])
+        if @growing_zone
+            @plants = @growing_zone.get_plants.order(:name)
+        else
+            redirect_to growing_zones_path
+        end
+    end
+
 end
