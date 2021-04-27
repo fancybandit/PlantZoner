1dbbb41859afafa4884f80b18aebc70d02c66c2c
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 00:37:29 -0400

    Handle login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,25 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to root_path
     end
+
+    def create
+        user = User.find_by(username: session_params[:username])
+        if user && user.authenticate(session_params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            if user
+                flash[:error] = "Invalid password."
+            else
+                flash[:error] = "Invalid username."
+            end
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :password)
+    end
 end
