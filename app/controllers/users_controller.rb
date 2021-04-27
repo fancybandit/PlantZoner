7308ea8d0675591464a781c9af0d263b1cdfc033
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 00:56:48 -0400

    Relocate sessions helpers to ApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
     def show
+        redirect_if_not_logged_in
         @user = User.find_by(id: params[:id])
         redirect_to '/' if !@user
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def redirect_if_not_logged_in
-        redirect_to root_path if !logged_in?
-    end
-
 end
