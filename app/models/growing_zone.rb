c135d2612ac86b8660f2b0b39957f20ea5f1ebc5
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 18:13:12 -0400

    Add #get_plants GrowingZone helper from mini proj

--- app/models/growing_zone.rb
+++ app/models/growing_zone.rb
@@ -2,4 +2,20 @@ class GrowingZone < ApplicationRecord
     has_many :gardens
     has_many :plants, through: :gardens
     has_many :owners, class_name: "User", through: :gardens
+
+    def get_plants
+        plants_hash = API.new.get_plants_data(self.zone_id)
+        plants_hash.each do |p|
+            plant = Plant.find_by(name: p[0])
+            if plant
+                self.plants << plant if self.plants.none? {|p| p == plant}
+            else
+                plant = Plant.new(name: p[0], scientific_name: p[1][:scientific_name], image_link: p[1][:image_link])
+                self.plants << plant
+                plant.save
+            end
+        end
+        self.plants
+    end
+    
 end
