c4e9f63ac5481146359a63613e764da3023e12d2
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 14:31:54 -0400

    Add bare actions/views for creating a garden

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,11 @@
 class GardensController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @garden = Garden.new
+    end
+
+    def create
+    end
+
 end
