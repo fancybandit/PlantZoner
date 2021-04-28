ae43e30f332904d0a981238cce7ccde9d3a3d6da
Jacob Klimenko <droidedjake@gmail.com>
2021-04-28 17:56:17 -0400

    Add validations to Garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,9 @@ class Garden < ApplicationRecord
   belongs_to :owner, class_name: "User"
   belongs_to :growing_zone
   belongs_to :plant
+
+  validates :name, presence: true
+  validates :owner_id, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :plant_id, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :growing_zone_id, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
