b948d2d799be04ea769997cd4a28ab1cf7869fa8
Jacob Klimenko <droidedjake@gmail.com>
2021-04-27 23:26:16 -0400

    Add slug methods to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,14 @@ class UsersController < ApplicationController
         @user = User.find_by(id: params[:id])
     end
 
+    def slug
+        self.username.gsub(/[^0-9A-Za-z ]/, '').downcase.split(' ').join('-')
+    end
+    
+    def find_by_slug(slug)
+        self.all.detect do |obj|
+            obj.slug == slug
+        end
+    end
+
 end
