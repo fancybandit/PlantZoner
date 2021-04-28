346a6bd574456b856f2d7ca2e85f7c7f6d841ecb
Jacob Klimenko <droidedjake@gmail.com>
2021-04-28 17:48:46 -0400

    Add validation to Plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,6 @@ class Plant < ApplicationRecord
     has_many :gardens
     has_many :growing_zones, through: :gardens
     has_many :owners, class_name: "User", through: :gardens
+
+    validates :scientific_name, presence: true
 end
