30fd9d0c28473f422bea4935dc19c65d704d4b7b
Jacob Klimenko <droidedjake@gmail.com>
2021-05-01 21:27:33 -0400

    Complete GitHub OAuth Login & control flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
     def home
+        session.clear if !current_user
     end
 
     def destroy
@@ -10,11 +11,15 @@ class SessionsController < ApplicationController
 
     def create
         if auth
+            user = find_or_create_user_by_auth_uid(auth['uid'])
+            user.save
+            session[:user_slug] = user.slug
+            redirect_to user_path(user.slug)
         else
             user = User.find_by(username: session_params[:username])
             if user && user.authenticate(session_params[:password])
                 session[:user_slug] = user.slugify
-                redirect_to user_path(user.slug)
+                redirect_to user_path(user)
             else
                 if user
                     flash[:error] = "Invalid password."
@@ -34,6 +39,23 @@ class SessionsController < ApplicationController
 
     def auth
         request.env['omniauth.auth']
+    end
+
+    def find_or_create_user_by_auth_uid(uid)
+        User.find_or_create_by(uid: uid) do |u|
+            if User.find_by(username: auth['info']['nickname'])
+                u.username = "#{auth['info']['nickname']}_GitHub"
+            else
+                u.username = auth['info']['nickname']
+            end
+            if auth['info']['email']
+                u.email = auth['info']['email']
+            else
+                u.email = "#{uid}@plantzoner.com"
+            end
+            u.password = ENV['AUTH_USER_PASSWORD']
+            u.slug = auth['info']['nickname'].gsub(/[^0-9A-Za-z ]/, '').downcase.split(' ').join('-')
+        end
     end
 
 end
